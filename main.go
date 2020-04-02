b9f168dc505b3ac45b6c20154d13ff03a8d00510
jinyunx <jinyunx@foxmail.com>
2020-04-02 09:21:18 +0800

    add syscall

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ typedef struct ds {
 	char *ptr;
 	int size;
 } ds;
-
 extern void my_callback(void*);
 static void my_job() {
   for (int i = 0; i < 10000000; ++i) {
@@ -16,35 +15,66 @@ static void my_job() {
 	my_callback(&d);
   }
 }
-
-static ds GetData() {
+static void GetData() {
 	ds d;
 	d.ptr = data;
 	d.size = 1024;
-	return d;
+	return;
 }
 */
 import "C"
 import (
 	"log"
+	"syscall"
 	"time"
 	"unsafe"
 )
 
+var (
+	dll = syscall.MustLoadDLL("dll.dll")
+
+	procDllGetData = dll.MustFindProc("DllGetData")
+	procDllJob     = dll.MustFindProc("DllJob")
+	procDllSet     = dll.MustFindProc("DllSet")
+)
+
 func main() {
+	//// cgo start
 	t := time.Now()
 	C.my_job()
-	log.Println(time.Since(t))
+	log.Println("cgo callback:", time.Since(t))
 
 	t = time.Now()
 	i := 10000000
 	for i > 0 {
 		C.GetData()
 		//log.Println(d)
-		i = i-1
+		i = i - 1
 	}
-	log.Println(time.Since(t))
+	log.Println("cgo call:", time.Since(t))
 
+	//// syscall
+	callback := syscall.NewCallback(go_callback)
+	_, _, _ = procDllSet.Call(callback)
+	t = time.Now()
+	_, _, _ = procDllJob.Call()
+	log.Println("syscall callback:", time.Since(t))
+
+	t = time.Now()
+	i = 10000000
+	for i > 0 {
+		_, _, _ = procDllGetData.Call()
+		//log.Println(d)
+		i = i - 1
+	}
+	log.Println("syscall call:", time.Since(t))
+
+}
+
+func go_callback(p unsafe.Pointer) (ret uintptr) {
+	//log.Println(p)
+	ret = 0
+	return ret
 }
 
 //export my_callback
